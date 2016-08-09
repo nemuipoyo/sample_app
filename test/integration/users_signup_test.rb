bca9db06a49f049730fe87ef460431e0ce2f1826
Sayaka Sugimoto <nemui@ht.sfc.keio.ac.jp>
2016-08-09 11:33:07 +0900

    Finish exercise 7.7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,17 +10,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
    					     	   password: "foo",
   						       password_confirmation: "bar" }
     end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+  end
+
+
 
   test "valid signup information" do
   	get signup_path
   	assert_difference 'User.count', 1 do
-  	  post_via_redirect user_path, user: { name: "Example User",
+  	  post_via_redirect users_path, user: { name: "Example User",
   	  									   email: "user@example.com",
   	  									   password: "password",
   	  									   password_confirmation: "password"}
   	end
-
-    assert_template 'users/new'
+    assert_template 'users/show'
+    assert_not flash.nil?
   end
 
 end
